379a1baec0abf80da0e11aceb30fcdcee793b244
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-02-14 08:22:21 -0500

    Create promiseChain.js

---
+++ promiseChain.js
@@ -0,0 +1,21 @@
+let link = state => {
+  return new Promise(function(resolve, reject) {
+    if (state) { 
+      resolve('success'); 
+    } else { 
+      reject('error');
+    }
+  });
+}
+ 
+let promiseChain = link(true);
+ 
+promiseChain
+.then( data => {  
+   console.log(data + " 1");
+   return link(true);
+})
+.then( data => {
+   console.log(data+ " 2");
+   return link(true);
+});
